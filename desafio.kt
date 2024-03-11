df8ec656dfdef00713b2544f88ad932fc1dbdb93
André Ricardo Morais <146028970+Andrersm@users.noreply.github.com>
2024-03-11 18:58:54 -0300

    Ajustes Desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,58 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, var email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int, var nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>,) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    fun listarinscritos() {
+        println("os incritos no curso de $nome são")
+        for(i in inscritos)
+        	println(i.nome)
+        	
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val user = Usuario("andré", "andre.mv.2@hotmail.com")
+    val user2 = Usuario("bruno", "bruno@gmail.com")
+    
+    println("os usuarios $user e $user2 foram cadastrados")
+    
+    val kot = ConteudoEducacional(nome = "Poo com kotlin", duracao = 10, nivel = Nivel.BASICO)
+    val kot2 = ConteudoEducacional(nome = "Poo com kotlin2", duracao = 15, nivel = Nivel.INTERMEDIARIO)
+    val kot3 = ConteudoEducacional(nome = "Poo com kotlin3", duracao = 30, nivel = Nivel.DIFICIL)
+    
+    val python = ConteudoEducacional(nome = "Poo com python", duracao = 5, nivel = Nivel.DIFICIL)
+    val python2 = ConteudoEducacional(nome = "Poo com python2", duracao = 30, nivel = Nivel.DIFICIL)
+    val python3 = ConteudoEducacional(nome = "Poo com python3", duracao = 20, nivel = Nivel.DIFICIL)
+  
+    
+    val cont = listOf<ConteudoEducacional>(kot, kot2, kot3)
+    val cont2 = listOf<ConteudoEducacional>(python, python2, python3)
+    
+    val formation = Formacao("Kotlin POO", conteudos = cont)
+    val formation2 = Formacao("Python POO", conteudos = cont2)
+    
+    formation.matricular(user)
+    formation.matricular(user2)
+    
+    formation2.matricular(user)
+    formation2.matricular(user2)
+    
+    println(formation)
+    println(formation2)
+    
+    formation.listarinscritos()
+    formation2.listarinscritos()
+    
+    
+    
+    
 }
